52d5085f528365612934999e61654e04f24c05f1
Oliver Cvetkoski <cvetbt@gmail.com>
2023-10-28 20:56:19 +0200

    finished project

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 // Get references to various elements on the page.
 
-const hamburgerMenu = document.querySelector(".hamburgerMenu");
-const menuItems = document.querySelector(".menuItems");
 const textOutput = document.querySelector(".textOutput");
 const form = document.querySelector(".form");
 const input = document.querySelector(".textInput");
 const copyrightDiv = document.querySelector(".copyright");
 const darkModeBtn = document.querySelector(".toggleBgColorBtn");
 const formSubmitBtn = document.querySelector(".formSubmitBtn");
-const bar1 = document.querySelector(".bar1");
-const bar2 = document.querySelector(".bar2");
-const bar3 = document.querySelector(".bar3");
 
 // Getting the current year and displaying it in the footer.
 
@@ -24,43 +19,6 @@ form.addEventListener("submit", (e) => {
   textOutput.innerHTML += `<p>${input.value}</p>`;
   input.value = "";
 });
-
-// Function for toggling the hamburger menu on screens smaller than 768px.
-
-const hamburgerDiv = document.createElement("div");
-hamburgerDiv.classList.add("hamburgerContainer");
-
-let isMenuOpen = false;
-let originalParent;
-
-function toggleHamburgerMenu() {
-  if (isMenuOpen) {
-    bar1.style.transform = "rotate(0deg)";
-    bar1.style.position = "static";
-    bar2.style.transform = "rotate(0deg)";
-    bar3.style.opacity = 1;
-
-    hamburgerDiv.classList.remove("showHamburgerMenu");
-    if (originalParent) {
-      originalParent.appendChild(menuItems);
-    }
-    document.body.removeChild(hamburgerDiv);
-  } else {
-    bar1.style.transform = "rotate(45deg)";
-    bar1.style.position = "absolute";
-    bar1.style.top = "1.9em";
-    bar2.style.transform = "rotate(-45deg)";
-    bar3.style.opacity = 0;
-
-    originalParent = menuItems.parentElement;
-    hamburgerDiv.classList.add("showHamburgerMenu");
-    hamburgerDiv.appendChild(menuItems);
-    document.body.appendChild(hamburgerDiv);
-  }
-  isMenuOpen = !isMenuOpen;
-}
-
-hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
 
 // Function for toggling between dark mode and light mode.
 
